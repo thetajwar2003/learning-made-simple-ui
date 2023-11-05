bfb33fa6c35b9938b13cd90bb481943e47c53ef3
Nafis Khan <49691160+nkhan-godaddy@users.noreply.github.com>
2023-11-05 15:40:49 -0500

    add course cards

--- src/app/student/courses.tsx
+++ src/app/student/courses.tsx
@@ -1,8 +1,30 @@
+import courses from '../../mock/courses.json';
+
 function Courses() {
   return (
     <div className='p-4 sm:ml-64'>
-      <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 bg-red-500'>
-        Courses
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
+        {courses.map((course, index) => (
+          <div
+            key={index}
+            className='max-w-sm bg-white border border-gray-200 rounded-lg shadow'
+          >
+            <a href='#'>
+              <img
+                className='rounded-t-lg'
+                src={course.bannerURL}
+                alt={course.courseName}
+              />
+              <div className='p-4'>
+                <h3 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                  {course.courseName}
+                </h3>
+                <p className='text-gray-700 font-bold'>{course.courseCode}</p>
+                <p className='text-gray-700'>{course.teacher}</p>
+              </div>
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
