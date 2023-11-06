c0e5395e662df7b3e8b6e070a4b8bf681ee89830
Tajwar Rahman <34196741+thetajwar2003@users.noreply.github.com>
2023-11-06 16:17:05 -0500

    Revert "Add Multiple Features"

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,30 +1,5 @@
-'use client';
-import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
-import { useState } from 'react';
-import Courses from './courses';
-import Dashboard from './dashboard';
+import React from "react";
 
-function Student() {
-  const [activeComponent, setActiveComponent] = useState('dashboard');
-
-  const renderComponent = () => {
-    switch (activeComponent) {
-      case 'dashboard':
-        return <Dashboard />;
-      case 'courses':
-        return <Courses />;
-      default:
-        return <Dashboard />;
-    }
-  };
-  return (
-    <div>
-      <Header />
-      <Sidebar setActiveComponent={setActiveComponent} />
-      {renderComponent()}
-    </div>
-  );
+export default function StudentDashboard() {
+  return <div>StudentDashboard</div>;
 }
-
-export default Student;
