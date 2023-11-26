610a8bc3d3ed730863fce818a168334b82bf979a
thetajwar2003 <rahmantajwar131@gmail.com>
2023-11-26 01:49:34 -0500

    added assignment card to post

--- src/components/CustomHero/UserHero.tsx
+++ src/components/CustomHero/UserHero.tsx
@@ -4,17 +4,23 @@ import React, { ReactNode } from "react";
 interface UserHeroProps {
   size?: string;
   comment?: boolean;
+  assignment?: boolean;
   children: ReactNode;
 }
 
 export default function UserHero({
   size = "16",
   comment = false,
+  assignment = false,
   children,
 }: UserHeroProps) {
   return (
     <>
-      <div className={`h-full flex ${comment ? null : "items-center"} mb-4`}>
+      <div
+        className={`h-full flex ${comment ? "items-start" : "items-center"} ${
+          assignment ? null : "mb-4"
+        }`}
+      >
         <img
           alt="team"
           className={`w-${size} h-${size} object-cover object-center flex-shrink-0 rounded-full mr-4 p-2`}
--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -2,6 +2,7 @@
 import PostCard from "@/components/CustomCards/PostCard";
 import ClassCodeCard from "@/components/CustomCards/ClassCodeCard";
 import React from "react";
+import AssignmentCard from "@/components/CustomCards/AssignmentCard";
 
 export default function Posts() {
   const data = {
@@ -15,6 +16,7 @@ export default function Posts() {
       id: "2",
       body: "Post content with comments",
       timestamp: "9:39 PM",
+      type: "announcement",
       originalPoster: "TEACH #01",
       comments: [
         {
@@ -30,13 +32,25 @@ export default function Posts() {
           poster: "TEACH #01",
         },
       ],
+      assignmentTitle: "",
     },
     {
       id: "1",
       body: "Your post content goes here...",
       timestamp: "November 22, 2023",
+      type: "announcement",
       originalPoster: "TEACH #01",
       comments: [],
+      assignmentTitle: "",
+    },
+    {
+      id: "0",
+      body: "Your post content goes here...",
+      timestamp: "November 19, 2023",
+      type: "assignment",
+      originalPoster: "TEACH #01",
+      comments: [],
+      assignmentTitle: "HW #2",
     },
 
     // ...more posts
@@ -68,8 +82,15 @@ export default function Posts() {
             </div>
           </button>
           <>
+            {/* TODO ERROR HERE */}
             {posts.map((item) => (
-              <PostCard {...item} />
+              <>
+                {item.type == "announcement" ? (
+                  <PostCard {...item} />
+                ) : (
+                  <AssignmentCard {...item} />
+                )}
+              </>
             ))}
           </>
         </div>
