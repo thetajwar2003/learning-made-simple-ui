9e624b4c449dff599aced15108bda6c304cb2ea8
nafisk <nafisrizwank@gmail.com>
2023-12-02 11:43:39 -0500

    Implement assignment tab

--- src/components/CustomCards/AssignmentCard.tsx
+++ src/components/CustomCards/AssignmentCard.tsx
@@ -1,30 +1,48 @@
-import React from "react";
-import UserHero from "../CustomHero/UserHero";
+import React from 'react';
+import UserHero from '../CustomHero/UserHero';
 
 interface AssignmentCardProps {
   id: string;
-  body: string;
-  timestamp: string;
-  originalPoster: string;
-  assignmentTitle: string;
+  title: string;
+  description: string;
+  dueDate: string;
+  postedDate: string;
+  teacher: {
+    name: string;
+    profilePicUrl: string;
+  };
+  fileUrls?: string[];
 }
 
 export default function AssignmentCard({
   id,
-  body,
-  timestamp,
-  originalPoster,
-  assignmentTitle,
+  title,
+  description,
+  dueDate,
+  postedDate,
+  teacher,
+  fileUrls = [],
 }: AssignmentCardProps) {
   return (
-    <div className="p-2 w-full" key={id}>
-      <div className="w-full p-2 rounded-lg border-2 border-secondary flex flex-col relative overfow-hidden">
-        <UserHero assignment={true}>
-          <div className="flex-grow">
-            <h2 className="text-white title-font text-sm">
-              {originalPoster} posted a new assignment: {assignmentTitle}
-            </h2>
-            <h3 className="text-gray-500 text-xs">{timestamp}</h3>
+    <div className='p-2 w-full' key={id}>
+      <div className='w-full p-2 rounded-lg border-2 border-secondary flex flex-col relative overflow-hidden'>
+        <UserHero assignment={true} profilePicUrl={teacher.profilePicUrl}>
+          {' '}
+          {/* Pass the teacher's profile picture URL */}
+          <div className='flex-grow'>
+            <h2 className='text-white title-font text-[1.5rem]'>{title}</h2>
+            <h3 className='text-gray-400 text-s'>Due Date: {dueDate}</h3>
+            <p className='text-gray-500'>{description}</p>
+            {fileUrls.map((url, index) => (
+              <a
+                key={index}
+                href={url}
+                className='text-blue-500 hover:underline'
+              >
+                Download File {index + 1}
+                <br />
+              </a>
+            ))}
           </div>
         </UserHero>
       </div>
--- src/sections/Assignments.tsx
+++ src/sections/Assignments.tsx
@@ -1,60 +1,15 @@
 'use client';
 import React from 'react';
 
-import PostCard from '@/components/CustomCards/PostCard';
-import ClassCodeCard from '@/components/CustomCards/ClassCodeCard';
 import AssignmentCard from '@/components/CustomCards/AssignmentCard';
-import CreateAnnouncementCard from '@/components/CustomCards/AnnouncementCard';
+import ClassCodeCard from '@/components/CustomCards/ClassCodeCard';
+import assignments from '@/mock/assignment.json'; // Assuming assignments are part of the posts.json
 
 export default function Assignments() {
   const data = {
     classCode: '3nmf91',
   };
-  const posts = [
-    // TODO: can be sorted when queried from dynamodb -> make timestamp the sort key
-    {
-      id: '2',
-      body: 'Post content with comments',
-      timestamp: '9:39 PM',
-      type: 'announcement',
-      originalPoster: 'TEACH #01',
-      comments: [
-        {
-          id: '2.1',
-          body: 'This is a comment',
-          timestamp: '10:00 PM',
-          poster: 'TEACH #01',
-        },
-        {
-          id: '2.2',
-          body: 'This is latest comment',
-          timestamp: '11:00 PM',
-          poster: 'TEACH #01',
-        },
-      ],
-      assignmentTitle: '',
-    },
-    {
-      id: '1',
-      body: 'Your post content goes here...',
-      timestamp: 'November 22, 2023',
-      type: 'announcement',
-      originalPoster: 'TEACH #01',
-      comments: [],
-      assignmentTitle: '',
-    },
-    {
-      id: '0',
-      body: 'Your post content goes here...',
-      timestamp: 'November 19, 2023',
-      type: 'assignment',
-      originalPoster: 'TEACH #01',
-      comments: [],
-      assignmentTitle: 'HW #2',
-    },
 
-    // ...more posts
-  ];
   return (
     <>
       <div className='grid grid-cols-5 gap-3'>
@@ -63,18 +18,9 @@ export default function Assignments() {
         </div>
 
         <div className='col-span-4 flex flex-col w-full'>
-          <CreateAnnouncementCard />
-          <>
-            {posts.map((item, index) => (
-              <>
-                {item.type == 'announcement' ? (
-                  <PostCard {...item} key={index} />
-                ) : (
-                  <AssignmentCard {...item} key={index} />
-                )}
-              </>
-            ))}
-          </>
+          {assignments.map((item, index) => (
+            <AssignmentCard {...item} key={index} />
+          ))}
         </div>
       </div>
     </>
