0361f78c6c9e76708e75c67c1d153a8ef4c3cb98
nafisk <nafisrizwank@gmail.com>
2023-12-03 22:38:39 -0500

    Update Header component with cursor-pointer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center '>
         <Link
           href={'/'}
-          className='flex title-font font-medium items-center text-white mb-4 md:mb-0'
+          className='flex title-font font-medium items-center text-white mb-4 md:mb-0 cursor-pointer'
           id='clickable-logo'
         >
           {/* TODO: replace the scg with an actual logo of our product */}
