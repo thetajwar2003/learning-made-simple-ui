3aab9a374cb20c156adfe655d06388418f04c962
nafisk <nafisrizwank@gmail.com>
2023-12-03 20:49:17 -0500

    Add features card and update nav bar with Link tags

---
+++ src/components/HomePage/Features.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import FeatureCard from './FeatureCard';
+import { GiTeacher, GiTeamIdea, GiChecklist } from 'react-icons/gi'; // Import your icons
+
+const Features = () => {
+  return (
+    <div className='container mx-auto'>
+      <div className='flex flex-wrap justify-center gap-4'>
+        <FeatureCard
+          title='Course Management'
+          description='Organize courses, share materials, and streamline class management.'
+          IconComponent={GiTeacher}
+        />
+        <FeatureCard
+          title='Student Collaboration'
+          description='Facilitate student interaction with forums, group projects, and peer reviews.'
+          IconComponent={GiTeamIdea}
+        />
+        <FeatureCard
+          title='Grade Tracking'
+          description='Easily track and report student grades and progress.'
+          IconComponent={GiChecklist}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Features;
