f19ab3790b8df9567c2d84257ffee0afc342fb83
thetajwar2003 <rahmantajwar131@gmail.com>
2023-12-04 18:07:19 -0500

    changed the grade component for dynamic user

--- src/sections/Grades.tsx
+++ src/sections/Grades.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import grades from '@/mock/grades.json';
-import StudentGrades from '@/components/CustomCards/StudentGrades';
-import TeacherGrades from '@/components/CustomCards/TeacherGrades';
+import React from "react";
+import grades from "@/mock/grades.json";
+import StudentGrades from "@/components/CustomCards/StudentGrades";
+import TeacherGrades from "@/components/CustomCards/TeacherGrades";
+import { usePathname } from "next/navigation";
+import { extractRole } from "../../utils/extractFromString";
 
-const userType: string = 'Teacher'; // Change to 'Teacher' for testing the teacher view
-const currentStudentID = 'STU#001'; // Current student ID (for testing)
+const userType: string = "Student"; // Change to 'Teacher' for testing the teacher view
+const currentStudentID = "STU#001"; // Current student ID (for testing)
 
 const Grades = ({ classCode }: any) => {
+  const pathname = usePathname();
+  const role = extractRole(pathname);
+
   const studentGrades = grades.find(
-    item => item.studentID === currentStudentID
+    (item) => item.studentID === currentStudentID
   );
 
   return (
     <>
-      {userType === 'Student' && (
+      {role === "student" && (
         <StudentGrades classCode={classCode} studentGrades={studentGrades} />
       )}
-      {userType === 'Teacher' && (
+      {role === "teacher" && (
         <TeacherGrades classCode={classCode} allGrades={grades} />
       )}
     </>
