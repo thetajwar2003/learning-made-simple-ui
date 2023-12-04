77eb20e1f72ee05e2ebb3a9c15dd8fd1b8777162
nafisk <nafisrizwank@gmail.com>
2023-12-04 15:49:04 -0500

    Implement grades for student and teachers

---
+++ src/components/CustomCards/TeacherGrades.tsx
@@ -0,0 +1,196 @@
+import React from 'react';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface TeacherGradesProps {
+  classCode: string;
+  allGrades: any[];
+}
+
+const TeacherGrades = ({ classCode, allGrades }: TeacherGradesProps) => {
+  // Function to convert letter grades to numerical values
+  const convertGradeToNumber = grade => {
+    switch (grade) {
+      case 'A+':
+        return 4.0;
+      case 'A':
+        return 3.8;
+      case 'A-':
+        return 3.7;
+      case 'B+':
+        return 3.5;
+      case 'B':
+        return 3.0;
+      case 'B-':
+        return 2.7;
+      case 'C+':
+        return 2.3;
+      case 'C':
+        return 2.0;
+      case 'C-':
+        return 1.7;
+      case 'D+':
+        return 1.3;
+      case 'D':
+        return 1.0;
+      case 'D-':
+        return 0.7;
+      case 'F':
+        return 0.0;
+      default:
+        return 0.0; // Or some default value
+    }
+  };
+
+  // Function to calculate average grade for each assignment
+  const calculateAverageGrades = () => {
+    const assignmentGrades = {};
+
+    allGrades.forEach(student => {
+      student.grades.forEach(grade => {
+        if (!assignmentGrades[grade.assignmentID]) {
+          assignmentGrades[grade.assignmentID] = {
+            title: grade.title,
+            description: grade.description,
+            total: 0,
+            count: 0,
+          };
+        }
+        const numericGrade = convertGradeToNumber(grade.grade);
+        assignmentGrades[grade.assignmentID].total += numericGrade;
+        assignmentGrades[grade.assignmentID].count += 1;
+      });
+    });
+
+    return Object.values(assignmentGrades).map(assignment => ({
+      ...assignment,
+      average: (assignment.total / assignment.count).toFixed(2),
+    }));
+  };
+
+  const averageGrades = calculateAverageGrades();
+
+  const chartOptions = {
+    scales: {
+      x: {
+        display: true, // Display the x-axis
+        ticks: {
+          color: 'white', // X-axis labels color
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.1)', // X-axis grid line color
+        },
+      },
+      y: {
+        display: true, // Display the y-axis
+        ticks: {
+          color: 'white', // Y-axis labels color
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.1)', // Y-axis grid line color
+        },
+      },
+      x1: {
+        display: false, // Hide the second x-axis if it exists
+      },
+      y1: {
+        display: false, // Hide the second y-axis if it exists
+      },
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: 'white', // Legend text color
+        },
+      },
+    },
+  };
+
+  // Prepare data for the bar chart (average grades)
+  const barChartData = {
+    labels: averageGrades.map(grade => grade.title),
+    datasets: [
+      {
+        label: 'Average Grades',
+        data: averageGrades.map(grade => grade.average),
+        backgroundColor: 'rgb(117,254,255)',
+      },
+    ],
+  };
+
+  // Prepare data for the line chart (grade trend)
+  const lineChartData = {
+    labels: averageGrades.map((_, index) => `Assignment ${index + 1}`),
+    datasets: [
+      {
+        label: 'Grade Trend',
+        data: averageGrades.map(grade => grade.average),
+        fill: false,
+        borderColor: 'rgb(117,254,255)',
+        tension: 0.1,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <div className='p-2 w-full flex m-2'>
+        <div className='w-1/2 p-4'>
+          <Bar data={barChartData} options={chartOptions} />
+        </div>
+        <div className='w-1/2 p-4'>
+          <Line data={lineChartData} options={chartOptions} />
+        </div>
+      </div>
+
+      <div className='w-full p-4 rounded-lg border-2 border-secondary flex flex-col relative overflow-hidden'>
+        <table className='w-full'>
+          <thead>
+            <tr>
+              <th className='text-left'>Assignment</th>
+              <th className='text-left'>Description</th>
+              <th className='flex justify-center'>Average Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {averageGrades.map((grade, index) => (
+              <tr
+                key={index}
+                className='hover:bg-gray-700 cursor-pointer rounded-lg'
+              >
+                <td className='py-2 px-4'>{grade.title}</td>
+                <td className='py-2 px-4'>{grade.description}</td>
+                <td className='py-2 px-4 flex justify-center'>
+                  {grade.average}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TeacherGrades;
--- src/sections/Grades.tsx
+++ src/sections/Grades.tsx
@@ -1,206 +1,26 @@
 import React from 'react';
-import { Bar, Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
 import grades from '@/mock/grades.json';
+import StudentGrades from '@/components/CustomCards/StudentGrades';
+import TeacherGrades from '@/components/CustomCards/TeacherGrades';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend
-);
+const userType = 'Teacher'; // Change to 'Teacher' for testing the teacher view
+const currentStudentID = 'STU#001'; // Current student ID (for testing)
 
-interface CourseProps {
-  classCode: string;
-}
-
-// Current student ID (for testing)
-const currentStudentID = 'STU#001';
-
-// Component for the Grades tab
-export default function Grades({ classCode }: CourseProps) {
-  // Find the grades for the current student
+const Grades = ({ classCode }: any) => {
   const studentGrades = grades.find(
     item => item.studentID === currentStudentID
   );
 
-  const chartOptions = {
-    scales: {
-      x: {
-        display: true, // Display the x-axis
-        ticks: {
-          color: 'white', // X-axis labels color
-        },
-        grid: {
-          color: 'rgba(255, 255, 255, 0.1)', // X-axis grid line color
-        },
-      },
-      y: {
-        display: true, // Display the y-axis
-        ticks: {
-          color: 'white', // Y-axis labels color
-        },
-        grid: {
-          color: 'rgba(255, 255, 255, 0.1)', // Y-axis grid line color
-        },
-      },
-      x1: {
-        display: false, // Hide the second x-axis if it exists
-      },
-      y1: {
-        display: false, // Hide the second y-axis if it exists
-      },
-    },
-    plugins: {
-      legend: {
-        labels: {
-          color: 'white', // Legend text color
-        },
-      },
-    },
-  };
-
-  // Function to convert letter grades to numerical values
-  const convertGradeToNumber = grade => {
-    switch (grade) {
-      case 'A+':
-        return 4.0;
-      case 'A':
-        return 3.8;
-      case 'A-':
-        return 3.7;
-      case 'B+':
-        return 3.5;
-      case 'B':
-        return 3.0;
-      case 'B-':
-        return 2.7;
-      case 'C+':
-        return 2.3;
-      case 'C':
-        return 2.0;
-      case 'C-':
-        return 1.7;
-      case 'D+':
-        return 1.3;
-      case 'D':
-        return 1.0;
-      case 'D-':
-        return 0.7;
-      case 'F':
-        return 0.0;
-      default:
-        return 0.0; // Or some default value
-    }
-  };
-
-  // Example usage:
-  const gradeAPlus = convertGradeToNumber('A+'); // Returns 4.0
-  const gradeCMinus = convertGradeToNumber('C-'); // Returns 1.7
-  const gradeZ = convertGradeToNumber('Z'); // Returns 0.0 (default)
-
-  // Prepare data for the bar chart (individual assignment grades)
-  const barChartData = {
-    labels: studentGrades?.grades.map(grade => grade.title),
-    datasets: [
-      {
-        label: 'Grades',
-        data: studentGrades?.grades.map(grade =>
-          convertGradeToNumber(grade.grade)
-        ),
-        backgroundColor: 'rgb(117,254,255)',
-      },
-    ],
-  };
-
-  // Prepare data for the line chart (grade trend)
-  const lineChartData = {
-    labels: studentGrades?.grades.map((_, index) => `Assignment ${index + 1}`),
-    datasets: [
-      {
-        label: 'Grade Trend',
-        data: studentGrades?.grades.map(grade =>
-          convertGradeToNumber(grade.grade)
-        ),
-        fill: false,
-        borderColor: 'rgb(117,254,255)',
-        tension: 0.1,
-      },
-    ],
-  };
-
   return (
     <>
-      {/* chart */}
-      <div className='p-2 w-full flex m-2'>
-        <div className='w-1/2 p-4'>
-          <Bar data={barChartData} options={chartOptions} />
-        </div>
-        <div className='w-1/2 p-4'>
-          <Line data={lineChartData} options={chartOptions} />
-        </div>
-      </div>
-
-      {/* grades tables */}
-      <div className='p-2 w-full'>
-        <div className='w-full p-4 rounded-lg border-2 border-secondary flex flex-col relative overflow-hidden'>
-          <div className='flex items-center justify-between'>
-            <div className='flex-grow'>
-              <h2 className='text-white title-font text-[1.5rem] mb-2'>
-                Grades for course: {classCode}
-              </h2>
-              <table className='w-full'>
-                <thead>
-                  <tr>
-                    <th className='text-left'>Assignment</th>
-                    <th className='text-left'>Description</th>
-                    <th className='flex justify-center'>Grade</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {studentGrades &&
-                    studentGrades.grades.map((grade: any, index) => (
-                      <tr
-                        key={index}
-                        className='hover:bg-gray-700 cursor-pointer rounded-lg'
-                      >
-                        <td className='py-2 px-4'>
-                          <a href='#' className='block text-white'>
-                            {grade.title}
-                          </a>
-                        </td>
-                        <td className='py-2 px-4'>
-                          <a href='#' className='block text-white'>
-                            {grade.description}
-                          </a>
-                        </td>
-                        <td className='py-2 px-4 flex justify-center'>
-                          <a href='#' className='block text-white'>
-                            {grade.grade}
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      {userType === 'Student' && (
+        <StudentGrades classCode={classCode} studentGrades={studentGrades} />
+      )}
+      {userType === 'Teacher' && (
+        <TeacherGrades classCode={classCode} allGrades={grades} />
+      )}
     </>
   );
-}
+};
+
+export default Grades;
